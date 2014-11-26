51efbb7d5025525a643e6eed6bb18fd1fd91362a
Peter Mihalik <peter.mihalik@gmail.com>
2014-11-26 12:37:15 +0100

    step 1

---
+++ RomanConverter/RomanConverter/ConvertToRomanExampleTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanConverter
+{
+	[TestClass]
+	public class ConvertToRomanExampleTest
+	{
+		[TestMethod]
+		public void ShouldConvertFromArabic()
+		{
+		}
+	}
+}
