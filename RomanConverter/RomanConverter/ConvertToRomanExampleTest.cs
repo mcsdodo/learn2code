71dca8ac2342cd0abbc5fbcb3a276b8bb115ec02
Peter Mihalik <peter.mihalik@gmail.com>
2014-11-26 12:39:26 +0100

    step 2

--- RomanConverter/RomanConverter/ConvertToRomanExampleTest.cs
+++ RomanConverter/RomanConverter/ConvertToRomanExampleTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RomanConverter
 {
@@ -8,6 +10,22 @@ namespace RomanConverter
 		[TestMethod]
 		public void ShouldConvertFromArabic()
 		{
+			Assert.AreEqual("I", ConvertToRoman(1));
+			Assert.AreEqual("II", ConvertToRoman(2));
+			Assert.AreEqual("III", ConvertToRoman(3));
+		}
+
+		private string ConvertToRoman(int number)
+		{
+		    if (number == 2)
+		    {
+		        return "II";
+		    }
+		    if (number == 3)
+		    {
+		        return "III";
+		    }
+		    return "I";
 		}
 	}
 }
