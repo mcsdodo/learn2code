b97be06efe498ab2ef85f104c3ca5f0882c714d6
Peter Mihalik <peter.mihalik@gmail.com>
2014-11-26 12:52:23 +0100

    step 5

--- RomanConverter/RomanConverter/ConvertToRomanExampleTest.cs
+++ RomanConverter/RomanConverter/ConvertToRomanExampleTest.cs
@@ -4,49 +4,44 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RomanConverter
 {
-	[TestClass]
-	public class ConvertToRomanExampleTest
-	{
-		[TestMethod]
-		public void ShouldConvertFromArabic()
-		{
-			Assert.AreEqual("I", ConvertToRoman(1));
-			Assert.AreEqual("II", ConvertToRoman(2));
-			Assert.AreEqual("III", ConvertToRoman(3));
+    [TestClass]
+    public class ConvertToRomanExampleTest
+    {
+        [TestMethod]
+        public void ShouldConvertFromArabic()
+        {
+            Assert.AreEqual("I", ConvertToRoman(1));
+            Assert.AreEqual("II", ConvertToRoman(2));
+            Assert.AreEqual("III", ConvertToRoman(3));
 
-			Assert.AreEqual("V", ConvertToRoman(5));
-			Assert.AreEqual("VI", ConvertToRoman(6));
-			Assert.AreEqual("VIII", ConvertToRoman(8));
+            Assert.AreEqual("V", ConvertToRoman(5));
+            Assert.AreEqual("VI", ConvertToRoman(6));
+            Assert.AreEqual("VIII", ConvertToRoman(8));
 
-			Assert.AreEqual("X", ConvertToRoman(10));
-			Assert.AreEqual("XX", ConvertToRoman(20));
-			Assert.AreEqual("XXX", ConvertToRoman(30));
-		}
+            Assert.AreEqual("X", ConvertToRoman(10));
+            Assert.AreEqual("XX", ConvertToRoman(20));
+            Assert.AreEqual("XXX", ConvertToRoman(30));
 
-		private string ConvertToRoman(int number)
-		{
-		    if (number == 10)
-		    {
-		        return "X";
-		    }
-		    string retVal = string.Empty;
+            Assert.AreEqual("XXV", ConvertToRoman(25));
+        }
 
-		    if (number > 10)
-		    {
-		        return "X".JoinTimes(number / 10);
-		    }
+        private string ConvertToRoman(int number)
+        {
+            string retVal = string.Empty;
 
-		    if (number > 5)
-		    {
-		        retVal = "V";
-		        number = number - 5;
-		    }
+            if (number >= 10)
+            {
+                retVal = "X".JoinTimes(number / 10);
+                number = number % 10;
+            }
 
-		    if (number == 5)
-		    {
-		        return "V";
-		    }
-		    return retVal + "I".JoinTimes(number);
-		}
-	}
+            if (number >= 5)
+            {
+                retVal += "V";
+                number = number % 5;
+            }
+
+            return retVal + "I".JoinTimes(number);
+        }
+    }
 }
